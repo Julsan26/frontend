9a42abc042a78ef47a66d05c3322a5c7d79d0d1a
Julsan Magaju <julsanmagaju@julsans-mbp.lan>
2023-01-21 20:53:09 -0600

    sucess page working

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,44 +1,99 @@
-import Stripe from "stripe"
+import Stripe from "stripe";
+const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
+import { getSession } from "@auth0/nextjs-auth0";
 
-const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`)
+export default async function handler(req, res) {
+  const session = getSession(req, res);
+  const user = session?.user;
+  console.log(user);
+  if (user) {
+    const stripeId = user["http://localhost:3000/stripe_customer_id"];
+    if (req.method === "POST") {
+      try {
+        const body = JSON.parse(req.body)
 
+        // Create Checkout Sessions from body params.
+        const session = await stripe.checkout.sessions.create({
+          submit_type: "pay",
+          mode: "payment",
+          payment_method_types: ["card"],
+          customer: stripeId,
+          shipping_address_collection: {
+            allowed_countries: ["US", "CA"],
+          },
 
-export default async function handler(req,res){
-    if(req.method=="POST"){
-        console.log(req.body)
-        try {
-
-
-          const body = JSON.parse(req.body)
-
-    
-          const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
-            shipping_address_collection: {allowed_countries: ['US', 'CA']},
-        
-            line_items: body.map(item=>{
-                return{
-
-                  price_data:{
-                      currency: 'usd',
-                      product_data:{
-                          name:item.title,
-                          images: [item.image.data.attributes.formats.thumbnail.url],
-                      },
-                      unit_amount:item.price * 100
-                  },
-                  quantity:item.quantity,
-                }
-            }),
-            mode: 'payment',
-            success_url: 'http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url: 'https://example.com/cancel',
-          });
-
-
-            res.status(200).json(session)
-        } catch (error) { 
-            res.status(error.statusCode || 500).json(error.message) }
-       
+          allow_promotion_codes: true,
+          line_items: body.map(item=> {
+            return {
+              price_data: {
+                currency: "usd",
+                product_data: {
+                  name: item.title,
+                  images: [item.image.data.attributes.formats.thumbnail.url],
+                },
+                unit_amount: item.price * 100,
+              },
+              adjustable_quantity: {
+                enabled: true,
+                minimum: 1,
+              },
+              quantity: item.quantity,
+            };
+          }),
+          success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${req.headers.origin}/canceled`,
+        });
+        res.status(200).json(session);
+      } catch (err) {
+        res.status(err.statusCode || 500).json(err.message);
+      }
+    } else {
+      res.setHeader("Allow", "POST");
+      res.status(405).end("Method Not Allowed");
     }
+  } else {
+    console.log("nope");
+    if (req.method === "POST") {
+      try {
+        const body = JSON.parse(req.body)
+
+        // Create Checkout Sessions from body params.
+        const session = await stripe.checkout.sessions.create({
+          submit_type: "pay",
+          mode: "payment",
+          payment_method_types: ["card"],
+          shipping_address_collection: {
+            allowed_countries: ["US", "CA"],
+          },
+
+          allow_promotion_codes: true,
+          line_items: body.map(item => {
+            return {
+              price_data: {
+                currency: "usd",
+                product_data: {
+                  name: item.title,
+                  images: [item.image.data.attributes.formats.thumbnail.url],
+                },
+                unit_amount: item.price * 100,
+              },
+              adjustable_quantity: {
+                enabled: true,
+                minimum: 1,
+              },
+              quantity: item.quantity,
+            };
+          }),
+          success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${req.headers.origin}/canceled`,
+        });
+        res.status(200).json(session);
+      } catch (err) {
+        res.status(err.statusCode || 500).json(err.message);
+      }
+    } else {
+      res.setHeader("Allow", "POST");
+      res.status(405).end("Method Not Allowed");
+    }
+  }
 }
